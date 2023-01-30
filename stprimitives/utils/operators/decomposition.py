cb90cdb8ddaefcf4b099deffa06bb820b7fdf486
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2023-01-30 09:23:36 +0100

    feat(utils.operators): add decomposition submodule

---
+++ stprimitives/utils/operators/decomposition.py
@@ -0,0 +1,108 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Quantum operators decomposition utils."""
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+from numpy import logical_or
+from qiskit.opflow import PauliSumOp
+from qiskit.primitives.utils import init_observable
+from qiskit.quantum_info.operators import Pauli, PauliList, SparsePauliOp
+from qiskit.quantum_info.operators.base_operator import BaseOperator
+
+
+################################################################################
+## OPERATOR DECOMPOSER INTERFACE
+################################################################################
+class OperatorDecomposer(ABC):
+    """Strategy interface for decomposing operators and getting associated measurement bases."""
+
+    def decompose(self, operator: BaseOperator | PauliSumOp | str) -> tuple[SparsePauliOp, ...]:
+        """Decomposes a given operator into singly measurable components.
+
+        Note that component decomposition is not unique, for instance, commuting components
+        could be grouped together in different ways (i.e. partitioning the set).
+
+        Args:
+            operator: the operator to decompose into its core components.
+
+        Returns:
+            A list of operators each of which measurable with a single quantum circuit
+            (i.e. on a singlet Pauli basis).
+        """
+        operator = init_observable(operator)
+        return self._decompose(operator)
+
+    @abstractmethod
+    def _decompose(
+        self,
+        operator: SparsePauliOp,
+    ) -> tuple[SparsePauliOp, ...]:
+        """Input-standardized version of `decompose`."""
+
+    def extract_pauli_bases(self, operator: BaseOperator | PauliSumOp | str) -> PauliList:
+        """Extract Pauli bases for a given operator.
+
+        Note that the resulting basis may be overcomplete depending on the implementation.
+
+        Args:
+            operator: an operator for which to obtain a Pauli basis for measurement.
+
+        Returns:
+            A `PauliList` of operators serving as a basis for the input operator. Each
+            entry conrresponds one-to-one to the components retrieved from `.decompose()`.
+        """
+        components = self.decompose(operator)
+        paulis = tuple(self._extract_singlet_pauli_basis(component) for component in components)
+        return PauliList(paulis)  # TODO: Allow `PauliList` from generator (i.e. Qiskit-Terra)
+
+    @abstractmethod
+    def _extract_singlet_pauli_basis(self, operator: SparsePauliOp) -> Pauli:
+        """Extract singlet Pauli basis for a given operator.
+
+        The input operator comes from `._decompose()`, and must be singly measurable.
+        """
+
+
+################################################################################
+## IMPLEMENTATIONS
+################################################################################
+class NaiveDecomposer(OperatorDecomposer):
+    """Trivial operator decomposition without grouping components."""
+
+    def _decompose(
+        self,
+        operator: SparsePauliOp,
+    ) -> tuple[SparsePauliOp, ...]:
+        return tuple(operator)
+
+    def _extract_singlet_pauli_basis(self, operator: SparsePauliOp) -> Pauli:
+        return operator.paulis[0]
+
+
+class AbelianDecomposer(OperatorDecomposer):
+    """Abelian operator decomposition grouping commuting components."""
+
+    def _decompose(
+        self,
+        operator: SparsePauliOp,
+    ) -> tuple[SparsePauliOp, ...]:
+        components = operator.group_commuting(qubit_wise=True)
+        return tuple(components)
+
+    def _extract_singlet_pauli_basis(self, operator: SparsePauliOp) -> Pauli:
+        or_reduce = logical_or.reduce
+        zx_data_tuple = or_reduce(operator.paulis.z), or_reduce(operator.paulis.x)
+        return Pauli(zx_data_tuple)
---
+++ test/utils/operators/test_decomposition.py
@@ -0,0 +1,147 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Tests for quantum operators decomposition utils."""
+
+from __future__ import annotations
+
+from pytest import mark
+from qiskit.opflow import PauliSumOp
+from qiskit.quantum_info.operators import Pauli, PauliList, SparsePauliOp
+
+from stprimitives.utils.operators import AbelianDecomposer, NaiveDecomposer
+
+
+################################################################################
+## TESTS
+################################################################################
+@mark.parametrize("decomposer", [NaiveDecomposer(), AbelianDecomposer()])
+class TestOperatorDecomposer:
+    """Test OperatorDecomposer."""
+
+    @mark.parametrize(
+        "unnormlized, normalized",
+        [
+            ("IXYZ", SparsePauliOp("IXYZ")),
+            (PauliList(["IXYZ", "ZYXI"]), SparsePauliOp(["IXYZ", "ZYXI"])),
+            (PauliSumOp(SparsePauliOp(["IXYZ", "IXII"])), SparsePauliOp(["IXYZ", "IXII"])),
+            (
+                PauliSumOp(SparsePauliOp(["IXYZ", "ZYXI", "IXII", "ZYII"])),
+                SparsePauliOp(["IXYZ", "ZYXI", "IXII", "ZYII"]),
+            ),
+        ],
+    )
+    def test_operator_normalization(self, decomposer, unnormlized, normalized):
+        """Test that input operators are normalized appropriately."""
+        assert decomposer.decompose(unnormlized) == decomposer.decompose(normalized)
+
+    @mark.parametrize(
+        "operator",
+        [
+            SparsePauliOp("IXYZ"),
+            SparsePauliOp(["IXYZ", "ZYXI"]),
+            SparsePauliOp(["IXYZ", "IXII"]),
+            SparsePauliOp(["IXYZ", "ZYXI", "IXII", "ZYII"]),
+        ],
+    )
+    def test_singlet_pauli_basis(self, decomposer, operator):
+        """Test that each component from decomposition produces only one basis."""
+        components = decomposer.decompose(operator)
+        assert len(decomposer.extract_pauli_bases(operator)) == len(components)
+        for component in components:
+            bases = decomposer.extract_pauli_bases(component)
+            assert len(bases) == 1
+
+
+class TestNaiveDecomposer:
+    """Test NaiveDecomposer."""
+
+    @mark.parametrize(
+        "operator, expected",
+        [
+            [SparsePauliOp("IXYZ"), (SparsePauliOp("IXYZ"),)],
+            [SparsePauliOp(["IXYZ", "ZYXI"]), (SparsePauliOp("IXYZ"), SparsePauliOp("ZYXI"))],
+            [SparsePauliOp(["IXYZ", "IXII"]), (SparsePauliOp("IXYZ"), SparsePauliOp("IXII"))],
+            [
+                SparsePauliOp(["IXYZ", "ZYXI", "IXII", "ZYII"]),
+                (
+                    SparsePauliOp("IXYZ"),
+                    SparsePauliOp("ZYXI"),
+                    SparsePauliOp("IXII"),
+                    SparsePauliOp("ZYII"),
+                ),
+            ],
+        ],
+    )
+    def test_decompose(self, operator, expected):
+        """Test decompose in ObservableDecomposer strategies."""
+        decomposer = NaiveDecomposer()
+        components = decomposer.decompose(operator)
+        assert components == expected
+
+    @mark.parametrize(
+        "operator, expected",
+        [
+            [SparsePauliOp("IXYZ"), PauliList(Pauli("IXYZ"))],
+            [SparsePauliOp(["IXYZ", "ZYXI"]), PauliList([Pauli("IXYZ"), Pauli("ZYXI")])],
+            [SparsePauliOp(["IXYZ", "IXII"]), PauliList([Pauli("IXYZ"), Pauli("IXII")])],
+            [
+                SparsePauliOp(["IXYZ", "ZYXI", "IXII", "ZYII"]),
+                PauliList([Pauli("IXYZ"), Pauli("ZYXI"), Pauli("IXII"), Pauli("ZYII")]),
+            ],
+        ],
+    )
+    def test_pauli_bases(self, operator, expected):
+        """Test Pauli basis in ObservableDecomposer strategies."""
+        decomposer = NaiveDecomposer()
+        basis = decomposer.extract_pauli_bases(operator)
+        assert basis == expected
+
+
+class TestAbelianDecomposer:
+    """Test AbelianDecomposer."""
+
+    @mark.parametrize(
+        "operator, expected",
+        [
+            [SparsePauliOp("IXYZ"), (SparsePauliOp("IXYZ"),)],
+            [SparsePauliOp(["IXYZ", "ZYXI"]), (SparsePauliOp("IXYZ"), SparsePauliOp("ZYXI"))],
+            [SparsePauliOp(["IXYZ", "IXII"]), (SparsePauliOp(["IXYZ", "IXII"]),)],
+            [
+                SparsePauliOp(["IXYZ", "ZYXI", "IXII", "ZYII"]),
+                (SparsePauliOp(["IXYZ", "IXII"]), SparsePauliOp(["ZYXI", "ZYII"])),
+            ],
+        ],
+    )
+    def test_decompose(self, operator, expected):
+        """Test decompose in ObservableDecomposer strategies."""
+        decomposer = AbelianDecomposer()
+        components = decomposer.decompose(operator)
+        assert components == expected
+
+    @mark.parametrize(
+        "operator, expected",
+        [
+            [SparsePauliOp("IXYZ"), PauliList(Pauli("IXYZ"))],
+            [SparsePauliOp(["IXYZ", "ZYXI"]), PauliList([Pauli("IXYZ"), Pauli("ZYXI")])],
+            [SparsePauliOp(["IXYZ", "IXII"]), PauliList([Pauli("IXYZ")])],
+            [
+                SparsePauliOp(["IXYZ", "ZYXI", "IXII", "ZYII"]),
+                PauliList([Pauli("IXYZ"), Pauli("ZYXI")]),
+            ],
+        ],
+    )
+    def test_pauli_bases(self, operator, expected):
+        """Test Pauli basis in ObservableDecomposer strategies."""
+        decomposer = AbelianDecomposer()
+        basis = decomposer.extract_pauli_bases(operator)
+        assert basis == expected
