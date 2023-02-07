caa20565e73598ec3fb01769cecf1306dd4463f2
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2023-02-07 19:20:10 +0100

    feat(time): add elapsed time utils

--- staged_primitives/utils/time.py
+++ staged_primitives/utils/time.py
@@ -14,14 +14,38 @@
 
 from __future__ import annotations
 
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 ################################################################################
 ## UTILS
 ################################################################################
-def isotimestamp(timezone: bool = False) -> str:
+def isotimestamp(timezone: bool = False) -> str:  # pylint: disable=redefined-outer-name
     """Generate ISO-8601 timestamp."""
     if timezone:
         return f"{datetime.now().astimezone().isoformat()}"
     return f"{datetime.utcnow().isoformat()}Z"
+
+
+def elapsed_seconds(isotimestamp: str) -> int:  # pylint: disable=redefined-outer-name
+    """Elapsed time in seconds from input ISO-8601 timestamp."""
+    isotimestamp = isotimestamp.replace("Z", "+00:00")
+    elapsed = datetime.now(timezone.utc) - datetime.fromisoformat(isotimestamp)
+    return (elapsed.days * 24 * 3600) + elapsed.seconds
+
+
+def elapsed_minutes(isotimestamp: str) -> int:  # pylint: disable=redefined-outer-name
+    """Elapsed time in minutes from input ISO-8601 timestamp."""
+    return elapsed_seconds(isotimestamp) // 60
+
+
+def elapsed_hours(isotimestamp: str) -> int:  # pylint: disable=redefined-outer-name
+    """Elapsed time in hours from input ISO-8601 timestamp."""
+    return elapsed_minutes(isotimestamp) // 60
+
+
+def elapsed_days(isotimestamp: str) -> int:  # pylint: disable=redefined-outer-name
+    """Elapsed time in days from input ISO-8601 timestamp."""
+    isotimestamp = isotimestamp.replace("Z", "+00:00")
+    elapsed = datetime.now(timezone.utc) - datetime.fromisoformat(isotimestamp.strip("Z"))
+    return elapsed.days
--- test/unit/utils/test_time.py
+++ test/unit/utils/test_time.py
@@ -17,7 +17,15 @@ from __future__ import annotations
 from datetime import datetime
 from unittest.mock import patch
 
-from staged_primitives.utils.time import isotimestamp
+from pytest import mark
+
+from staged_primitives.utils.time import (
+    elapsed_days,
+    elapsed_hours,
+    elapsed_minutes,
+    elapsed_seconds,
+    isotimestamp,
+)
 
 
 ################################################################################
@@ -41,3 +49,105 @@ class TestISOTimeStamp:
             datetime_mock.now().astimezone.return_value = datetime.fromisoformat(expected)
             stamp = isotimestamp(timezone=True)
         assert stamp == expected
+
+
+class TestElapsedTime:
+    """Test elapsed time."""
+
+    @mark.parametrize(
+        "timestamp, now, expected",
+        [
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:04Z", 4),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:12Z", 12),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:01:00Z", 60),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:12:00Z", 60 * 12),
+            ("1970-01-01T00:00:00Z", "1970-01-01T02:00:00Z", 3600 * 2),
+            ("1970-01-01T00:00:00Z", "1970-01-02T00:00:00Z", 3600 * 24),
+            ("1970-01-01T00:00:00Z", "1970-02-01T00:00:00Z", 3600 * 24 * 31),
+        ],
+    )
+    def test_elapsed_seconds(self, timestamp, now, expected):
+        """Test elapsed seconds."""
+        now = now.replace("Z", "+00:00")
+        with patch("staged_primitives.utils.time.datetime") as datetime_mock:
+            datetime_mock.now.return_value = datetime.fromisoformat(now)
+            datetime_mock.fromisoformat.side_effect = datetime.fromisoformat
+            assert elapsed_seconds(timestamp) == expected
+
+    @mark.parametrize(
+        "timestamp, now, expected",
+        [
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:04Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:12Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:01:00Z", 1),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:12:00Z", 12),
+            ("1970-01-01T00:00:00Z", "1970-01-01T02:00:00Z", 60 * 2),
+            ("1970-01-01T00:00:00Z", "1970-01-02T00:00:00Z", 60 * 24),
+            ("1970-01-01T00:00:00Z", "1970-02-01T00:00:00Z", 60 * 24 * 31),
+        ],
+    )
+    def test_elapsed_minutes(self, timestamp, now, expected):
+        """Test elapsed minutes."""
+        now = now.replace("Z", "+00:00")
+        with patch("staged_primitives.utils.time.datetime") as datetime_mock:
+            datetime_mock.now.return_value = datetime.fromisoformat(now)
+            datetime_mock.fromisoformat.side_effect = datetime.fromisoformat
+            assert elapsed_minutes(timestamp) == expected
+
+    @mark.parametrize(
+        "timestamp, now, expected",
+        [
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:04Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:12Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:01:00Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:12:00Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T02:00:00Z", 2),
+            ("1970-01-01T00:00:00Z", "1970-01-02T00:00:00Z", 24),
+            ("1970-01-01T00:00:00Z", "1970-02-01T00:00:00Z", 24 * 31),
+        ],
+    )
+    def test_elapsed_hours(self, timestamp, now, expected):
+        """Test elapsed hours."""
+        now = now.replace("Z", "+00:00")
+        with patch("staged_primitives.utils.time.datetime") as datetime_mock:
+            datetime_mock.now.return_value = datetime.fromisoformat(now)
+            datetime_mock.fromisoformat.side_effect = datetime.fromisoformat
+            assert elapsed_hours(timestamp) == expected
+
+    @mark.parametrize(
+        "timestamp, now, expected",
+        [
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:04Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:12Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:01:00Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:12:00Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T02:00:00Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-02T00:00:00Z", 1),
+            ("1970-01-01T00:00:00Z", "1970-02-01T00:00:00Z", 31),
+        ],
+    )
+    def test_elapsed_days(self, timestamp, now, expected):
+        """Test elapsed days."""
+        now = now.replace("Z", "+00:00")
+        with patch("staged_primitives.utils.time.datetime") as datetime_mock:
+            datetime_mock.now.return_value = datetime.fromisoformat(now)
+            datetime_mock.fromisoformat.side_effect = datetime.fromisoformat
+            assert elapsed_days(timestamp) == expected
+
+    @mark.parametrize(
+        "timestamp, now, expected",
+        [
+            ("1970-01-01T00:00:00+00:00", "1970-01-01T00:00:00Z", 0),
+            ("1970-01-01T00:00:00Z", "1970-01-01T00:00:00+00:00", 0),
+            ("1970-01-01T00:00:00+01:00", "1970-01-01T00:00:00Z", 1),
+            ("1970-01-01T00:00:00Z", "1970-01-01T12:00:00+01:00", 11),
+            ("1970-01-01T00:00:00+01:00", "1970-01-01T00:00:00-05:00", 6),
+        ],
+    )
+    def test_timezone(self, timestamp, now, expected):
+        """Test elapsed time in different timezones."""
+        now = now.replace("Z", "+00:00")
+        with patch("staged_primitives.utils.time.datetime") as datetime_mock:
+            datetime_mock.now.return_value = datetime.fromisoformat(now)
+            datetime_mock.fromisoformat.side_effect = datetime.fromisoformat
+            assert elapsed_hours(timestamp) == expected
